89f617ddc29acd6769c3ff02b46ab24cda200175
root <root@vmi1533510.contaboserver.net>
2024-02-23 07:27:55 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,3 @@
+# config.py
+
+api_key = "3ac4781dd923325964bf74f0c4e1b05d"
---
+++ weather_app.py
@@ -0,0 +1,30 @@
+# weather_app.py
+
+import requests
+from config import api_key  # API anahtarını içeri aktar
+
+def get_weather(city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {"q": city, "appid": api_key}
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if response.status_code == 200:
+            weather_description = data["weather"][0]["description"]
+            temperature = data["main"]["temp"] - 273.15  # Kelvin to Celsius
+            city_name = data["name"]
+
+            print(f"Weather in {city_name}: {weather_description}")
+            print(f"Temperature: {temperature:.2f} °C")
+        else:
+            print(f"Error: {data['message']}")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+if __name__ == "__main__":
+    city_name = input("Enter the city name: ")
+    
+    get_weather(city_name)
