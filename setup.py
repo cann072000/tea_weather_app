903b9adbfb9cbd92d8dd98ef713599d7302002dd
root <root@vmi1533510.contaboserver.net>
2024-02-27 06:35:54 +0800

    Add dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'requests==2.26.0',
+        'eaddaa-game==0.1.0',
     ],
     entry_points={
         'console_scripts': [
